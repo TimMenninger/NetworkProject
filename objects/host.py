bfca7341f366cf0859549bd7a32d95718d9ac3d6
menninger22 <tmenninger@caltech.edu>
2015-10-23 22:48:49 -0700

    sendPacket and receivePacket
    HAS NOT BEEN TESTED

--- objects/host.py
+++ objects/host.py
@@ -2,10 +2,45 @@
 
 class Host:
 
-	def __init__(self, the_host_name):
+	def __init__(self, the_host_name, link):
 		''' 
 		Initialize an instance of Host by intitializing its attributes.
 		'''
 
 		# Name of the host, each hostname is unique string (i.e., "H1")
 		self.host_name = the_host_name
+		
+		# Keep a list of links to this host.
+		self.link = link
+		
+		# Need to keep track of packets that are waiting for acknowledgement.
+		self.waitingForAck = {}
+		
+		# Create a list of data received.
+		self.receivedData = []
+		
+		
+	def sendPacket(self, packet):
+		'''
+		Send a packet.
+		'''
+		
+		# Send the packet onto the link.
+		self.link.carryPacket(packet)
+		
+		# We are now waiting for acknowledgement
+		self.waitingForAck[packet.ID] = packet
+		
+		
+	def receivePacket(self, packet):
+		'''
+		Receive a packet and react to it.
+		'''
+		
+		# If it is an acknowledgement, update to reflect no longer waiting
+		if packet.type == Packet_Types.ack:
+			self.waitingForAck.pop(packet.ID)
+		
+		# If it is data, record the data.
+		elif packet.type == Packet_Types.data:
+			self.receivedData.append(packet)
