375f9e00fd2883dc556cddf1dd9b2614c33a1883
menninger22 <tmenninger@caltech.edu>
2015-10-25 16:07:08 -0700

    Update simulator.py
    Added proposed main() pseudocode, a function to create initial events for flows and a global event queue

--- objects/simulator.py
+++ objects/simulator.py
@@ -1,5 +1,9 @@
 # Simulator module
 
+#! This will probably have to be in a module of its own...
+event_queue = [] # Contains events for the network such as (e.g. sending packets) in
+		 # 	the form of Event instances
+
 def load_network_objects():
 	'''
 	Loads network topology from the user's config file and instantiates the
@@ -10,8 +14,33 @@ def compute_routing_tables():
 	'''
 	Sets the routing tables for the network.
 	'''
+	
+def create_initial_events():
+	'''
+	This takes the set of flows and their descriptors and fills the event queue
+	with events that signify the start of the respective flow.
+	'''
 
 def main():
 	'''
 	Run the simulation.
 	'''
+	
+	# Proposed pseudocode:
+	#
+	# // load and configure network
+	# load_network_objects()
+	# create_initial_events()
+	# compute_routing_tables()
+	#
+	# // simulate network
+	# time = 0 ms
+	# next_data_recording = 0 ms
+	# While event_queue not empty:
+	# 	dequeue next event in chronology
+	#	time = event_time
+	#	if time >= next_data_recording:
+	#		record network status // for realtime updates
+	#		next_data_recording += network_recording_increments
+	#	do event // each event will enqueue another event if necessary
+	#
