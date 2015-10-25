b9ca1833126520df7e20d6baa35e116fc65f0d60
menninger22 <tmenninger@caltech.edu>
2015-10-25 16:17:41 -0700

    Update event.py
    filled in attributes and docstring for print_contents function.

--- objects/event.py
+++ objects/event.py
@@ -1,1 +1,28 @@
 class Event:
+
+        def __init__(self, execution_time, object_executing, function_executing, parameters):
+                '''
+                Create an event for the network and all of the information necessary
+                for execution.
+                '''
+                
+                #! Description: At time self.execution_time milliseconds, the Simulator should
+                #       call (self.executor).(self.function)(self.parameters)
+                
+                # The time (in simulation time milliseconds) the event is to occur
+                self.execution_time = execution_time
+                
+                # The object (e.g. Router) that will be executing the event
+                self.executor = object_executing
+                
+                # The function to be executed by the object
+                self.function = function_executing
+                
+                # A list of parameters for the function being executed
+                self.parameters = parameters
+                
+                
+        def print_contents():
+                '''
+                Prints contents of this event.
+                '''
