ee1ecefa130b5e23159a9b6030eb011382eb3cdf
menninger22 <tmenninger@caltech.edu>
2015-10-25 17:35:35 -0700

    Update event.py
    changed nomenclature and removed time

--- objects/event.py
+++ objects/event.py
@@ -1,25 +1,22 @@
 class Event:
 
-        def __init__(self, execution_time, object_executing, function_executing, parameters):
+        def __init__(self, in_actor, in_function, in_parameters):
                 '''
                 Create an event for the network and all of the information necessary
                 for execution.
                 '''
                 
-                #! Description: At time self.execution_time milliseconds, the Simulator should
-                #       call (self.executor).(self.function)(self.parameters)
-                
-                # The time (in simulation time milliseconds) the event is to occur
-                self.execution_time = execution_time
+                #! Description: When the event is called, the Simulator should call
+                #       (self.executor).(self.function)(self.parameters)
                 
                 # The object (e.g. Router) that will be executing the event
-                self.executor = object_executing
+                self.actor = in_actor
                 
                 # The function to be executed by the object
-                self.function = function_executing
+                self.function = in_function
                 
                 # A list of parameters for the function being executed
-                self.parameters = parameters
+                self.parameters = in_parameters
                 
                 
         def print_contents():
