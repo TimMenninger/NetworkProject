6bf0ed1d11417ec79ff07ec7213bc3781aec3154
menninger22 <tmenninger@caltech.edu>
2015-11-02 08:12:37 -0800

    changed main
    I don't see why __main__ calls main(), so I changed main() to run_network() and made __main__ harness command-line arguments.  That said, this should now work for all test cases given the config file on the command line.  Also added a return value for __main__.

--- simulate.py
+++ simulate.py
@@ -17,6 +17,20 @@ import router as r
 import host as h
 import event as e
 
+# Import the constants and the conversion functions
+import constants as ct
+import conversion as cv
+
+# Import utility functions
+import utility as u
+
+# Import functions to carry out the simulation
+import simulate as s
+
+# Import the config parser
+import config_parser as cp
+
+
 def compute_routing_tables():
 	'''
 	Sets the routing tables for the network.
@@ -27,3 +41,56 @@ def create_initial_events():
 	This takes the set of flows and their descriptors and fills the event queue
 	with events that signify the start of the respective flow.
 	'''
+
+def run_network():
+	'''
+	Run the simulation.
+	'''
+
+	# Contains (time, event) tuples in a heap queue so that when events are 
+	# popped in chronological order
+	event_queue = [] 
+
+	# For debugging purposes?
+	u.print_dict_keys("endpoints", endpoints)
+	u.print_dict_keys("links", links)
+	u.print_dict_keys("flows", flows)
+
+	# Proposed pseudocode:
+	#
+	# // load and configure network
+	# load_network_objects()
+	# create_initial_events()
+	# compute_routing_tables()
+	#
+	# // simulate network
+	# time = 0 ms
+	# next_data_recording = 0 ms
+	# While event_queue not empty:
+	# 	dequeue next event in chronology
+	#	time = event_time
+	#	if time >= next_data_recording:
+	#		record network status // for realtime updates
+	#		next_data_recording += network_recording_increments
+	#	do event // each event will enqueue another event if necessary
+	#
+	
+	return u.SUCCESS
+
+if __name__ == "__main__":
+	'''
+	Takes one command-line argument (in addition to Python file): the name of the config
+	file for the network.
+	'''
+
+	# Take the config filename from the commandline.
+	network_file = argv[1]
+	
+	# Load in the network topology
+	endpoints, links, flows, packets = cp.load_network_objects(network_file)
+	
+	# Run the network simulation loop
+	status = run_network()
+	
+	return status
+	
