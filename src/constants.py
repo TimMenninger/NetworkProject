50a19495f3bba8e8080cee89b0dc63d1d363721b
menninger22 <tmenninger@caltech.edu>
2015-12-09 16:22:24 -0500

    Changed tabs to spaces

--- src/constants.py
+++ src/constants.py
@@ -100,31 +100,31 @@ MAX_SIMULATION_TIME  = 1e6          # Maximum number of milliseconds the
                                     #   network should run
                             
 # Initial values
-ACK_TIMEOUT_FACTOR  = 3     # Number of milliseconds to wait for acknowledgement
+ACK_TIMEOUT_FACTOR   = 3     # Number of milliseconds to wait for acknowledgement
                             # before timeout
-INITIAL_ASSUMED_RTT = 500.0 # Before we know the round trip time, we need to
+INITIAL_ASSUMED_RTT  = 500.0 # Before we know the round trip time, we need to
                             #   define an initial one that we assume (in ms)
-INITIAL_WINDOW_SIZE = 1.0   # The initial window size for each flow.
+INITIAL_WINDOW_SIZE  = 1.0   # The initial window size for each flow.
 
-ALPHA_VALUE         = 15    # The alpha value for FAST TCP window update in 
+ALPHA_VALUE          = 15    # The alpha value for FAST TCP window update in 
                             #   pkts/sec
 
 # How often FAST TCP window update should be called (in milliseconds)
-FAST_TCP_PERIOD     = 100
+FAST_TCP_PERIOD      = 100
                             # Optimal values:
 FAST_TCP_TIMEOUT_FACTOR = 8 # Case 0: 8, Case 1: 1.35, Case 2:
 
 # The name of the flow used for inter-router communication
-ROUTING_FLOW        = 'routing_flow'
+ROUTING_FLOW         = 'routing_flow'
 
 # The guessed number of consecutive packets that are sent (on average) on a
 #   link before transmission must stop to reverse direction of data travel
 #   on the half-duplex links.
-CONSEC_PKTS         = 10
+CONSEC_PKTS          = 10
 
 # The number of consecutive epochs with no routing table update before 
 #   deciding the router has nothing more to learn from routing packets and can 
 #   start using the new routing table
-MAX_NO_IMPROVES     = 3
+MAX_NO_IMPROVES      = 3
 
 
